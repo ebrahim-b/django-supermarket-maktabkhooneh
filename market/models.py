28d2979517bf73d97f0985d40eadd8983becdbec
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-03-29 18:26:45 +0430

    Add simple Models

--- market/models.py
+++ market/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    inventory = models.PositiveIntegerField(default=0, null=True)
+
+    def increase_inventory(self, amount):
+        pass
+
+    def decrease_inventory(self, amount):
+        pass
+
+
+class Customer(models.Model):
+    user = OneToOneField(User, on_delete = models.CASCADE)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    balance = models.PositiveIntegerField(default=20000, null=True)
+
+    def deposit(self, amount):
+        pass
+
+    def spend(self, amount):
+        pass
+
+
+class OrderRow(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+
+class Order(models.Model):
+    STATUS_SHOPPING = 1
+    STATUS_SUBMITTED = 2
+    STATUS_CANCELED = 3
+    STATUS_SENT = 4
+
+    status_choices = (
+        (STATUS_SHOPPING, "shopping"),
+        (STATUS_SUBMITTED, "submitted"),
+        (STATUS_CANCELED, "canceled"),
+        (STATUS_SENT, "sent"),
+    )
+    
+    customer = models.ForiegnKey(Customer, on_delete=models.CASCADE)
+    order_time = models.DateTimeField()
+    total_price = models.IntegerField()
+    status = models.IntegerField(choices=status_choices)
+
+    @staticmethod
+    def initiate(customer):
+        pass
+
+    def add_product(self, product, amount):
+        pass
+
+    def remove_product(self, product, amount=None):
+        pass
+
+    def submit(self):
+        pass
+
+    def cancel(self):
+        pass
+
+    def send(self):
+        pass
